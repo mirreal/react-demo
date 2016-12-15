76784a5aea76607aa477cb4ae05c040716981f8e
mirreal <zgkm181142521@gmail.com>
2016-12-15 18:20:45 +0800

    update

---
+++ src/list.js
@@ -0,0 +1,31 @@
+/**
+ * @fileOverview little walk
+ * @author mirreal
+ *
+ * walk the file directory
+ */
+
+const fs = require('fs')
+const path = require('path')
+
+
+let fileList = [];
+
+
+function walk(path) {
+    const dirList = fs.readdirSync(path);
+    dirList.forEach(function(item) {
+        if (fs.statSync(path + '/' + item).isDirectory() && item !== 'node_modules') {
+            console.log(item)
+
+            walk(path + '/' + item);
+        } else {
+            fileList.push(path + '/' + item);
+        }
+    });
+}
+
+const filePath = path.resolve();
+walk(filePath)
+
+console.log(fileList)
